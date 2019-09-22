35c89bc6eb9efb6af7b7c7d7f885adeb9ea5b582
Victor Korir <vkkorir@gmail.com>
2019-09-21 18:25:38 -0700

    fixed server failing build

--- spring/server/src/main/java/edu/berkeley/gpporgs/resolver/OrganizationResolver.java
+++ spring/server/src/main/java/edu/berkeley/gpporgs/resolver/OrganizationResolver.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class OrganizationResolver implements GraphQLResolver<Organization> {
