f03a33a5e100e87159e8032904742e65b35082e3
Victor Korir <vkkorir@gmail.com>
2019-12-09 16:00:17 -0800

    cleared out all the wordpress files

--- wordpress/app/sql/local.wp_users.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_users` VALUES
-(1,"gpporgs","$P$B8Eax7kbLOuJJdATqQ.BadmfkMSQdn1","gpporgs","vkkorir@gmail.com","","2019-07-15 16:49:33","",0,"gpporgs");
