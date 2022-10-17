08344e81fa509d7e2e79ee07a2f3069fb33a3d2a
Victor Korir <vkkorir@gmail.com>
2022-10-16 17:55:47 -0700

    upgrading angular client to latest version

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,36 +1,7 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AdminGuard } from './admin/admin.guard';
-import { AdminComponent } from './admin/admin.component';
-import { HomeGuard } from './home/home.guard';
-import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    component: HomeComponent,
-    canActivate: [HomeGuard],
-    canLoad: [HomeGuard]
-  },
-  {
-    path: 'admin',
-    pathMatch: 'full',
-    component: AdminComponent,
-    canActivate: [AdminGuard]
-  },
-  {
-    path: 'login',
-    pathMatch: 'full',
-    component: LoginComponent
-  },
-  {
-    path: '**',
-    redirectTo: ''
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,131 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { GraphQLModule } from "./graphql.module";
-import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
-import { HomeComponent } from "./home/home.component";
-import { HeaderComponent } from "./header/header.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
-import { TableComponent } from "./table/table.component";
-import { DetailsComponent } from "./details/details.component";
-import { AdminComponent } from "./admin/admin.component";
-import { LoginComponent } from "./login/login.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import {
-  MatButtonModule,
-  MatCheckboxModule,
-  MatDividerModule,
-  MatFormFieldModule,
-  MatGridListModule,
-  MatInputModule,
-  MatToolbarModule,
-  MatRadioModule,
-  MatTableModule,
-  MatPaginatorModule,
-  MatSortModule,
-  MatDialogModule,
-  MatSnackBarModule,
-  MatIconModule,
-  MatAutocompleteModule,
-  MatSelectModule,
-  MatExpansionModule,
-  MatSlideToggleModule,
-  MatChipsModule,
-  MatSliderModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatCardModule,
-} from "@angular/material";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { LayoutModule } from "@angular/cdk/layout";
-import { TokenInterceptor } from "./token.interceptor";
-import { AppService } from "./app.service";
-import { ReactiveFormsModule } from "@angular/forms";
-import { LookUpComponent } from "./look-up/look-up.component";
-import { RouteReuseStrategy } from "@angular/router";
-import { CacheRouteReuseStrategy } from "./cache-route-reuse.strategy";
-import { MainModalComponent } from "./main-modal/main-modal.component";
-import { AddUserComponent } from "./admin/add-user/add-user.component";
-import { ManageUsersComponent } from "./admin/manage-users/manage-users.component";
-import { ManageOrganizationsComponent } from "./admin/manage-organizations/manage-organizations.component";
-
-const materialModules = [
-  MatButtonModule,
-  MatCheckboxModule,
-  MatDividerModule,
-  MatFormFieldModule,
-  MatProgressSpinnerModule,
-  MatGridListModule,
-  MatInputModule,
-  MatToolbarModule,
-  MatRadioModule,
-  MatTableModule,
-  MatPaginatorModule,
-  MatSortModule,
-  MatDialogModule,
-  MatSnackBarModule,
-  MatIconModule,
-  MatAutocompleteModule,
-  MatSelectModule,
-  MatExpansionModule,
-  MatSlideToggleModule,
-  MatChipsModule,
-  MatSliderModule,
-  MatProgressBarModule,
-  MatCardModule,
-];
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    SidebarComponent,
-    TableComponent,
-    DetailsComponent,
-    AdminComponent,
-    LoginComponent,
-    LookUpComponent,
-    MainModalComponent,
-    AddUserComponent,
-    ManageUsersComponent,
-    ManageOrganizationsComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    GraphQLModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    FlexLayoutModule,
-    materialModules,
-    ReactiveFormsModule,
-    MatSelectModule,
+    AppRoutingModule
   ],
-  providers: [
-    AppService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptor,
-      multi: true,
-    },
-    {
-      provide: RouteReuseStrategy,
-      useClass: CacheRouteReuseStrategy,
-    },
-  ],
-  bootstrap: [AppComponent],
-  entryComponents: [
-    AdminComponent,
-    MainModalComponent,
-    LookUpComponent,
-    DetailsComponent,
-    AddUserComponent,
-    ManageUsersComponent,
-    ManageOrganizationsComponent,
-  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
